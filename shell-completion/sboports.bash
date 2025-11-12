65713c7fc143145084a6baedd9489111be310bba
rgcastrof <rogeriogirao1@proton.me>
2025-11-12 16:53:51 -0300

    finish initial completion

--- shell-completion/sboports.bash
+++ shell-completion/sboports.bash
@@ -2,6 +2,23 @@
 
 _sboports()
 {
-	local cur prev words cword
-	_init_completion || return
+	local cur prev avaliable_commands
+
+	cur=${COMP_WORDS[COMP_CWORD]}
+	prev=${COMP_WORDS[COMP_CWORD-1]}
+	avaliable_commands="fetch checksum diff install
+		uninstall clean search help"
+
+	if [ "$COMP_CWORD" -eq 1 ]; then
+		COMPREPLY=($(compgen -W "$avaliable_commands" -- $cur))
+		return 0
+	fi
+
+	case "$prev" in
+		fetch)
+			COMPREPLY=($(compgen -W "extract update" -- $cur))
+			;;
+	esac
 }
+
+complete -F _sboports sboports
